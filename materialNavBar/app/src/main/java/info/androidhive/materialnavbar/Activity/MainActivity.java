20bc816c160bd17233cc1891a5c6bcf299d9a15a
barthr <bartfokker@gmail.com>
2015-05-28 19:13:12 +0200

    NavigationDrawer with Categories
    ListView implemented, will tweak soon

---
+++ materialNavBar/app/src/main/java/info/androidhive/materialnavbar/Activity/MainActivity.java
@@ -0,0 +1,113 @@
+package info.androidhive.materialnavbar.Activity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import info.androidhive.materialnavbar.Fact;
+import info.androidhive.materialnavbar.JSON;
+import info.androidhive.materialnavbar.MenuList;
+import info.androidhive.materialnavbar.R;
+import info.androidhive.materialnavbar.ViewAdapters.ListViewMenuAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private JSON jsonObject = null;
+    private ArrayList<Fact> facts = null;
+    private ListViewMenuAdapter adapter = new ListViewMenuAdapter();
+
+
+    @Override
+    //Teset push
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager()
+                        .findFragmentById(R.id.drawer_fragment);
+        drawerFragment.setUp(R.id.drawer_fragment, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        StrictMode.ThreadPolicy old = StrictMode.getThreadPolicy();
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder(old)
+                .permitAll()
+                .build());
+        try {
+
+            testJSON();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ListView codeLearnLessons = (ListView)findViewById(R.id.listViewId);
+        codeLearnLessons.setAdapter(adapter);
+    }
+
+    private void testJSON() throws JSONException, ExecutionException, InterruptedException {
+        jsonObject = new JSON(getBaseContext(), findViewById(R.id.progressBar));
+        facts = jsonObject.getFactAllList();
+        fillLayout();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fillLayout() {
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.linearFactLayout);
+        if (facts != null) {
+            for (Fact fact : facts) {
+                TextView x = new TextView(findViewById(R.id.linearFactLayout).getContext());
+                String name = fact.getName();
+                String description = fact.getDescription();
+                x.setText(name + "\n" + description + "\n");
+                ll.addView(x);
+                Log.i("Fact", name);
+            }
+        } else {
+            Log.e("ERROR", "Factslist is empty");
+            try {
+                facts = jsonObject.getFactsList();
+                Log.d("facts", facts.get(0).getName());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ materialNavBar/app/src/main/java/info/androidhive/materialnavbar/ViewAdapters/ListViewMenuAdapter.java
@@ -0,0 +1,93 @@
+package info.androidhive.materialnavbar.ViewAdapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import info.androidhive.materialnavbar.MenuList;
+import info.androidhive.materialnavbar.R;
+
+/**
+ * Created by bart on 28-5-2015
+ * 2015 - 05
+ */
+
+
+public class ListViewMenuAdapter extends BaseAdapter {
+
+
+    private List<MenuList> codeLearnChapterList = getDataForMenu();
+
+    /**
+     * Passes the MenuList object to a List
+     *
+     * @return the List with MenuList objects
+     */
+    public List<MenuList> getDataForMenu() {
+        String[] category = {"Today", "Facts", "History", "Birthdays", "Lifehacks", "Quotes"}; //List of categories
+        int[] icons = {R.drawable.ic_birthdays, R.drawable.ic_facts, R.drawable.ic_history, R.drawable.ic_birthdays, R.drawable.ic_quotes, R.drawable.ic_quotes}; //List of Menu icons
+
+        List<MenuList> menuItems = new ArrayList<MenuList>();
+        for (int i = 0; i < 6; i++) {
+            MenuList m = new MenuList();
+            m.setIcon(icons[i]);
+            m.setCategory(category[i]);
+            menuItems.add(m);
+        }
+        return menuItems;
+    }
+
+
+    /**
+     * returns the size of the object list
+     */
+    @Override
+    public int getCount() {
+        return codeLearnChapterList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return codeLearnChapterList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    /**
+     * Method create's a view for the ListView
+     * @return View contains object MenuList which get placed inside the ListView
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //If the convertView equals null, there is no convertView already initialized
+        //otherwise the getView method "recycles" the convertView
+        //this is for the performance of the MenuList
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) LayoutInflater.from(parent.getContext()); //Inflate the layout if there is no convertView
+            convertView = inflater.inflate(R.layout.custom_row, parent, false); //set the contertView to the infalte layout with XML custom_row
+        }
+        ImageView icon = (ImageView) convertView.findViewById(R.id.list_icon); //findTheElementById
+        TextView category = (TextView) convertView.findViewById(R.id.list_title); //findTheElementById
+        MenuList chapter = codeLearnChapterList.get(position);
+
+        icon.setImageResource(chapter.getIcon());
+        category.setText(chapter.getCategory());
+
+        return convertView; //return the content view with the object
+    }
+}
